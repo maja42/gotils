8e25d7168bedc39d4365e61b1a40cf13c0526377
maja42 <2649265+maja42@users.noreply.github.com>
2021-05-21 11:32:52 -0700

    Added quoteJoin function

--- strings.go
+++ strings.go
@@ -1,6 +1,9 @@
 package gotils
 
-import "strings"
+import (
+	"strconv"
+	"strings"
+)
 
 // Indent the given string by an additional 4 spaces.
 // Returns an empty string if the input is also empty.
@@ -10,4 +13,13 @@ func Indent(s string) string {
 	}
 	prefix := "    "
 	return prefix + strings.ReplaceAll(s, "\n", "\n"+prefix)
+}
+
+// QuoteJoin quotes all strings and then joins them with sep.
+func QuoteJoin(s []string, sep string) string {
+	quoted := make([]string, len(s)) // copy (do not modify original slice)
+	for idx, str := range s {
+		quoted[idx] = strconv.Quote(str)
+	}
+	return strings.Join(quoted, sep)
 }
